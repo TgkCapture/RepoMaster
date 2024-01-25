69116bd3d98930ec2251dda86e875274687c53ea
TgkCapture <tawongakanyenda5@gmail.com>
2024-01-25 08:45:35 +0200

    corrected some blueprints

--- app/controllers/repo_controller.py
+++ app/controllers/repo_controller.py
@@ -1,22 +1,28 @@
-# repo_controller.py
+"""repo_controller.py
+"""
+import os
 import requests
 from flask import Blueprint, render_template, request
+from app.controllers.github_controller import get_github_repositories
 
 repo_controller = Blueprint('repo', __name__)
+
+github_username = os.environ.get('GITHUB_USERNAME')
+github_token = os.environ.get('GITHUB_TOKEN')
 
 @repo_controller.route('/repositories/<repo_name>/issues')
 def show_issues(repo_name):
 
     url = f'https://api.github.com/repos/{github_username}/{repo_name}/issues'
 
-    response = requests.get(url)
+    response = requests.get(url, timeout=60)
     if response.status_code == 200:
         issues = response.json()
         return render_template('issues.html', repo_name=repo_name, issues=issues)
     else:
         return f"Failed to fetch issues for {repo_name} from GitHub API"
 
-@main_routes.route('/delete_repo', methods=['GET', 'POST'])
+@repo_controller.route('/delete_repo', methods=['GET', 'POST'])
 def delete_repository():
     if request.method == 'POST':
         repos_to_delete = request.form.getlist('repo_to_delete[]')
@@ -30,7 +36,7 @@ def delete_repository():
                 'Accept': 'application/vnd.github.v3+json'
             }
 
-            response = requests.delete(delete_url, headers=headers)
+            response = requests.delete(delete_url, headers=headers, timeout=60)
 
             if response.status_code == 204:
                 deleted_repos.append(repo_name)
@@ -48,13 +54,13 @@ def delete_repository():
         else:
             return "Failed to Fetch Repos from Github"
 
-@main_routes.route('/repositories/<owner>/<repo_name>/pulls')
+@repo_controller.route('/repositories/<owner>/<repo_name>/pulls')
 def list_pull_requests(owner, repo_name):
 
     headers = {'Authorization': f'token {github_token}'}
 
     api_url = f'https://api.github.com/repos/{owner}/{repo_name}/pulls'
-    response = requests.get(api_url, headers=headers)
+    response = requests.get(api_url, headers=headers, timeout=60)
 
     if response.status_code == 200:
         pull_requests = response.json()
