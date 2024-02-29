6a92bcaee564b4787fb6bfe33c6ef43641d4d998
TgkCapture <tawongakanyenda5@gmail.com>
2024-02-29 21:11:03 +0200

    initalizing oauth lib

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 # app.py
 from flask import Flask
+from app.config.oauth_config import oauth
 from app.routes.main_routes import main_routes
 from app.config.development_config import DevelopmentConfig
 
 app = Flask(__name__, template_folder='app/templates')
 app.config.from_object(DevelopmentConfig)
+
+oauth.init_app(app)
 
 # Register the main_routes Blueprint
 app.register_blueprint(main_routes)
