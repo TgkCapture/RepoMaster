11690cf3a7d564b8357a3080578c240d615eda69
TgkCapture <tawongakanyenda5@gmail.com>
2024-05-05 21:51:18 +0200

    feat: Registered static dir and moved styling and scripts

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from app.routes.main_routes import main_routes
 from app.config.development_config import DevelopmentConfig
 
-app = Flask(__name__, template_folder='app/templates')
+app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 app.config.from_object(DevelopmentConfig)
 
 # Register the main_routes Blueprint
