88d8c2e47a3fc6b56c3bf2a2465ea7cd2415052e
TgkCapture <tawongakanyenda5@gmail.com>
2024-05-12 00:39:40 +0200

    feat: importing keys from dot-env

--- app/config/development_config.py
+++ app/config/development_config.py
@@ -1,5 +1,13 @@
 # development_config.py
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 class DevelopmentConfig:
     DEBUG = True
     SECRET_KEY = 'your_secret_key'
+
+    CLIENT_ID = os.getenv('consumer_key')
+    CLIENT_KEY = os.getenv('consumer_secret')
