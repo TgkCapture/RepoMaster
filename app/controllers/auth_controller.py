6c5da7db3e8a825789673ceef970d4b1f1f81755
TgkCapture <tawongakanyenda5@gmail.com>
2024-05-12 19:00:43 +0200

    feat: moved remote app to configs

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,26 +1,12 @@
 """auth_controller.py
 """
-import os
-import requests
-from flask import Flask, Blueprint, render_template, redirect, url_for, session, request
-from flask_oauthlib.client import OAuth
-from werkzeug.urls import url_encode, url_unquote, url_quote
+
+from flask import Blueprint, render_template, url_for, session, request
+from app.config.oauth_config import github
 
 auth_controller = Blueprint('authorize', __name__)
 auth_controller.secret_key = '123456'
 
-oauth = OAuth(auth_controller)
-github = oauth.remote_app(
-    'github',
-    consumer_key='',  
-    consumer_secret='',  
-    request_token_params={'scope': 'user:email'},
-    base_url='https://api.github.com/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://github.com/login/oauth/access_token',
-    authorize_url='https://github.com/login/oauth/authorize',
-)
 
 def index():
     return render_template('index.html')
@@ -28,14 +14,16 @@ def index():
 def login():
     if 'github_token' in session:
         # If user is already logged in, redirect to index
-        return redirect(url_for('index'))
+        # return redirect(url_for('main.home'))
+        return f"You are now signed in."
     
     # Redirect user to GitHub authorization page
     return github.authorize(callback=url_for('main.authorized_route', _external=True))
 
 def logout():
     session.pop('github_token', None)
-    return redirect(url_for('index'))
+    # return redirect(url_for('main.home'))
+    return f"You have now logged out"
 
 def authorized():
     error_reason = request.args.get('error_reason')
@@ -50,7 +38,8 @@ def authorized():
         return render_template('error.html', error_message=error_message)
     
     session['github_token'] = (resp['access_token'], '')
-    return redirect(url_for('index'))
+    # return redirect(url_for('main.home'))
+    return f"You are now signed in.."
 
 def get_github_oauth_token():
     return session.get('github_token')
