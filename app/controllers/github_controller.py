1a6ae63400e69efc74a418d677e3a71dd0b8ab34
TgkCapture <tawongakanyenda5@gmail.com>
2024-05-12 20:27:40 +0200

    feat: retrieving repos for only logged in users

--- app/controllers/github_controller.py
+++ app/controllers/github_controller.py
@@ -3,16 +3,16 @@ github_controller.py
 """
 import os
 import requests
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from app.controllers.auth_controller import get_github_oauth_token, get_user_info, is_user_logged_in
 
 github_controller = Blueprint('github', __name__)
 
-github_username = os.environ.get('GITHUB_USERNAME')
-github_token = os.environ.get('GITHUB_TOKEN')
 
-def get_github_repositories():
-    url = f'https://api.github.com/users/{github_username}/repos'
-    response = requests.get(url, timeout=60)
+def get_github_repositories(username, access_token):
+    url = f'https://api.github.com/users/{username}/repos'
+    headers = {'Authorization': f'token {access_token}'} if access_token else None
+    response = requests.get(url, headers=headers, timeout=60)
     if response.status_code == 200:
         return response.json()
     else:
@@ -20,8 +20,14 @@ def get_github_repositories():
 
 @github_controller.route('/repositories')
 def show_repositories():
-    repositories = get_github_repositories()
+    username, access_token = None, None
+
+    if is_user_logged_in(): 
+        username, _ = get_user_info()
+        access_token = get_github_oauth_token()
+
+    repositories = get_github_repositories(username, access_token)
     if repositories:
         return render_template('repositories.html', repositories=repositories)
     else:
-        return "Failed to Fetch repo from Github"
+        return render_template('error.html', error_message="Failed to fetch repositories")
