70091de1d4f382b2c4d46074221a78b37557ad48
TgkCapture <tawongakanyenda5@gmail.com>
2024-05-14 09:51:38 +0200

    feat: moved css and js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -32,3 +32,15 @@ document.getElementById('github-login').addEventListener('click', function(event
     event.preventDefault();
     signInWithGitHub();
 });
+
+$('[data-github]').each(function () {
+  var _this = this;
+  var repo = $(_this).data('github')
+
+  fetch('https://api.github.com/repos/' + repo).then(function (response) {
+      return response.json();
+  }).then(function (response) {
+      $(_this).find('[data-forks]').text(response.forks);
+      $(_this).find('[data-stars]').text(response.stargazers_count);
+  });
+  });
