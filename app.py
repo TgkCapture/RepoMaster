c2e8cac12b3febd58537f9a4530690a4696f4fc5
TgkCapture <tawongakanyenda5@gmail.com>
2024-05-23 13:10:23 +0200

    feat: configured logging mechanism

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 # app.py
 from flask import Flask
+import os
 from app.routes.main_routes import main_routes
 from app.config.development_config import DevelopmentConfig
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 app.config.from_object(DevelopmentConfig)
+
+# Call the logging setup function
+DevelopmentConfig.setup_logging()
 
 # Register the main_routes Blueprint
 app.register_blueprint(main_routes)
--- app/config/development_config.py
+++ app/config/development_config.py
@@ -1,7 +1,6 @@
-# development_config.py
-
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 
@@ -9,5 +8,25 @@ class DevelopmentConfig:
     DEBUG = True
     SECRET_KEY = 'your_secret_key'
 
-    CLIENT_ID = os.getenv('GITHUB_CONSUMER_KEY'),
+    CLIENT_ID = os.getenv('GITHUB_CONSUMER_KEY')
     CLIENT_KEY = os.getenv('GITHUB_CONSUMER_SECRET')
+
+    # Logging Configurations
+    LOG_LEVEL = logging.DEBUG
+    LOG_FILE_PATH = os.path.join('logs', 'repomaster.log')
+    LOG_FORMAT = '%(asctime)s %(levelname)s %(name)s: %(message)s'
+
+    @staticmethod
+    def setup_logging():
+        
+        log_dir = os.path.dirname(DevelopmentConfig.LOG_FILE_PATH)
+        try:
+            os.makedirs(log_dir, exist_ok=True)
+        except OSError as e:
+            print(f"Failed to create log directory: {e}")
+
+        logging.basicConfig(
+            level=DevelopmentConfig.LOG_LEVEL,
+            format=DevelopmentConfig.LOG_FORMAT,
+            filename=DevelopmentConfig.LOG_FILE_PATH
+        )
