0e57fa968227b5cb887aea3ccb7c0c0b55e38b15
TgkCapture <tawongakanyenda5@gmail.com>
2024-05-23 13:18:09 +0200

    removed comments

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ from app.config.development_config import DevelopmentConfig
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 app.config.from_object(DevelopmentConfig)
 
-# Call the logging setup function
 DevelopmentConfig.setup_logging()
 
-# Register the main_routes Blueprint
 app.register_blueprint(main_routes)
 
 if __name__ == '__main__':
