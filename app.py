5710fb5ba263bc7cc2f4000cb2f562d0007ac8da
TgkCapture <tawongakanyenda5@gmail.com>
2024-05-24 12:58:56 +0200

    feat: register error handler

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from flask import Flask
 import os
 from app.routes.main_routes import main_routes
 from app.config.development_config import DevelopmentConfig
+from app.config.error_handler import register_error_handlers
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 app.config.from_object(DevelopmentConfig)
 
 DevelopmentConfig.setup_logging()
+
+register_error_handlers(app)
 
 app.register_blueprint(main_routes)
 
