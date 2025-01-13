2a8250304be613eced8806cc9b965936a7493c8c
TgkCapture <tawongakanyenda5@gmail.com>
2025-01-13 23:11:23 +0200

    feat: retrieving private key from path

--- app/config/github_app_config.py
+++ app/config/github_app_config.py
@@ -9,4 +9,15 @@ app = Flask(__name__)
 load_dotenv()
 
 GITHUB_APP_ID = os.getenv('GITHUB_APP_ID')
-GITHUB_PRIVATE_KEY = os.getenv('GITHUB_PRIVATE_KEY')
+
+def load_private_key():
+    private_key_path = os.getenv('GITHUB_PRIVATE_KEY_PATH')
+    if private_key_path and os.path.exists(private_key_path):
+        with open(private_key_path, 'r') as f:
+            private_key = f.read()
+            return private_key
+    else:
+        raise FileNotFoundError(f"Private key file not found at {private_key_path}")
+
+# Load the private key
+GITHUB_PRIVATE_KEY = load_private_key()
