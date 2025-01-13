aac61132304e01137552ad3b3899bcdc55f77ebc
TgkCapture <tawongakanyenda5@gmail.com>
2025-01-13 20:43:57 +0200

    Feat: remove oauth config for new github app configuration

---
+++ app/config/github_app_config.py
@@ -0,0 +1,11 @@
+# github_app_config.py
+
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+load_dotenv()
+
+GITHUB_APP_ID = os.getenv('GITHUB_APP_ID')
+GITHUB_PRIVATE_KEY = os.getenv('GITHUB_PRIVATE_KEY')
