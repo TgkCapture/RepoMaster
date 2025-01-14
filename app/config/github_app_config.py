7955a2b2e40b7f7a8149f24407280ceba6927a78
TgkCapture <tawongakanyenda5@gmail.com>
2025-01-14 18:29:02 +0200

    freezed

--- app/config/github_app_config.py
+++ app/config/github_app_config.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 load_dotenv()
 
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
 
 GITHUB_APP_ID = os.getenv('GITHUB_APP_ID')
 
@@ -18,7 +18,7 @@ def load_private_key():
     if private_key_path and os.path.exists(private_key_path):
         with open(private_key_path, 'r') as f:
             private_key = f.read()
-            logging.info(f"Private key loaded successfully. Preview: {private_key[:20]}...{private_key[-20:]}")
+            # logging.info(f"Private key loaded successfully. Preview: {private_key[:20]}...{private_key[-20:]}")
             return private_key
     else:
         raise FileNotFoundError(f"Private key file not found at {private_key_path}")
